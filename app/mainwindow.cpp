5d44236bd240d5dfc84211e3475b36a17f2ae7de
Paul Gregory <pgregory@aqsis.org>
2009-11-10 22:48:57 +0000

    Connect tree selections to editor.
     * Double clicking on a model in the tree will load
       the body source into the editor.

--- app/mainwindow.cpp
+++ app/mainwindow.cpp
@@ -1,7 +1,10 @@
+#include <iostream>
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "avarlistmodel.h"
 #include "scenetreemodel.h"
+
 extern "C" {
 #include "lua.h"
 
@@ -11,7 +14,7 @@ extern "C" {
 
 
 MainWindow::MainWindow(lua_State *L, QWidget *parent)
-    : QMainWindow(parent), ui(new Ui::MainWindow)
+    : QMainWindow(parent), ui(new Ui::MainWindow), L(L)
 {
     ui->setupUi(this);
 
@@ -32,11 +35,12 @@ MainWindow::MainWindow(lua_State *L, QWidget *parent)
     sceneTreeView = ui->sceneTreeView;
     sceneTreeView->setModel(sceneModel);
 
+    QObject::connect(sceneTreeView, SIGNAL(doubleClicked(QModelIndex)), this, SLOT(selectModel(QModelIndex)));
+
     mainEditor = new QTextEdit;
     mainEditor = ui->mainEditor;
     highlighter = new LuaHighlighter(mainEditor->document());
 
-    // Now fill in the cameras from the Lua state.
     lua_getglobal(L, "Model");
     lua_getfield(L, -1, "models");
     lua_pushnil(L); /* the first key */
@@ -54,4 +58,20 @@ MainWindow::MainWindow(lua_State *L, QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::selectModel(const QModelIndex &index)
+{
+    std::cerr << "Row: " << index.row() << " Column: " << index.column() << std::endl;
+    lua_getglobal(L, "Model");
+    lua_getfield(L, -1, "models");
+    lua_pushnil(L); /* the first key */
+    int rowNo = index.row();
+    while(lua_next(L, -2) != 0 && --rowNo >= 0)
+        lua_pop(L, 1);
+    /* 'key' at -2, 'value' at -1 */
+    lua_getfield(L, -1, "bodySource");
+    const char* name = lua_tostring(L, -1);
+    mainEditor->setText(name);
+    lua_pop(L, 2);
 }
--- app/mainwindow.h
+++ app/mainwindow.h
@@ -5,6 +5,7 @@
 #include <QtGui/QSplitter>
 #include <QtGui/QTextEdit>
 #include <QtCore/QProcess>
+#include <QModelIndex>
 
 #include "luahighlighter.h"
 
@@ -23,11 +24,15 @@ public:
     MainWindow(lua_State *L, QWidget *parent = 0);
     ~MainWindow();
 
+public slots:
+    void selectModel(const QModelIndex& index);
+
 private:
     Ui::MainWindow *ui;
     QSplitter* splitter;
     QTextEdit* mainEditor;
     LuaHighlighter* highlighter;
+    lua_State* L;
 };
 
 #endif // MAINWINDOW_H
